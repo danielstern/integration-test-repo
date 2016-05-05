db08f464c6658294335b835a822c4e58bbfd2bca
Code Whisperer <daniel@danielstern.ca>
2016-05-05 10:35:06 -0400

    fix bug

--- webhook.js
+++ webhook.js
@@ -4,7 +4,8 @@ console.log("Running webapp...");
 
 let Express = require('express');
 let app = new Express();
-app.use(require('body-parser'));
+app.use(require('body-parser').urlencoded());
+app.use(require('body-parser').json());
 app.get('/',(req,res)=>{
 	console.log("User connecting to app");
 	res.status(200).send("Hello Node.js");
