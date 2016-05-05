e1ee57a1f2a4a282a77f1f3b148f91548f224641
Code Whisperer <daniel@danielstern.ca>
2016-05-05 10:15:23 -0400

    add body parser

--- webhook.js
+++ webhook.js
@@ -4,12 +4,14 @@ console.log("Running webapp...");
 
 let Express = require('express');
 let app = new Express();
+app.use(express.bodyParser());
 app.get('/',(req,res)=>{
 	console.log("User connecting to app");
 	res.status(200).send("Hello Node.js");
 })
 .post('/webhook',(req,res)=>{
 	console.log("Webhook activated");
+	console.log(req.body);
 	res.status(200).send();
 })
 .listen(process.env.PORT || 80,()=>{
